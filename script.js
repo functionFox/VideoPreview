172bf2ab613f779f0167989fff5dacc52bef7869
functionFox <110884035+functionFox@users.noreply.github.com>
2022-09-19 20:31:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var mainVideo = document.getElementById("mainVideo");
+
+function playVid() {
+    mainVideo.play();
+}
+
+function pauseVid() {
+    mainVideo.pause();
+}
